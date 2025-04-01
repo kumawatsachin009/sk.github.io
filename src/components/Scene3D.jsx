034281fbf722a90147759d202aa1ae81a685a1bf
sachinkumawatcoindcx <sachin.kumawat@coindcx.com>
2025-04-02 00:20:01 +0530

    update

--- src/components/Scene3D.jsx
+++ src/components/Scene3D.jsx
@@ -96,7 +96,7 @@ class Scene3D {
 
     loadModel() {
         const loader = new GLTFLoader();
-        const modelPath = '/astronaut.glb';
+        const modelPath = '/assets/astronaut.glb';
         
         console.log('Loading model from:', modelPath);
         
